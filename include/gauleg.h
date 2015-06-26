75332938999b88e903b9f8b45d84adaa5fc0e707
jbarakat <jbarakat@stanford.edu>
2015-06-26 13:54:59 -0700

    made header file for gauleg

---
+++ include/gauleg.h
@@ -0,0 +1,24 @@
+/*GAUSS-LEGENDRE QUADRATURE
+ * Generate the abscissas and weigts for Gauss-Legendre quadrature by
+ * solving an eigenvalue proglem for a symmetric, tridiagonal matrix.
+ *
+ * REFERENCES
+ *  Golub and Welsch, Mathematics of Computation 23-106 (1969)
+ * 
+ * PARAMETERS
+ *  n	[input]		number of points
+ *  X	[output]	abcsissas
+ *  W	[output]	weights
+ */
+
+#ifndef GAUSSLEG_H
+#define GAUSSLEG_H
+
+#include <lapacke.h>
+#include <cblas.h>
+#include <math.h>
+
+// function prototypes
+void gauleg(int, double*, double*);
+
+#endif
--- lib/gauleg.cpp
+++ lib/gauleg.cpp
@@ -1,4 +1,4 @@
-/*GAUSS-LEGENDRE QUADRATURE
+/* GAUSS-LEGENDRE QUADRATURE
  * Generate the abscissas and weigts for Gauss-Legendre quadrature by
  * solving an eigenvalue proglem for a symmetric, tridiagonal matrix.
  *
@@ -11,15 +11,7 @@
  *  W	[output]	weights
  */
 
-// this will go into the interface file eventually...
-//#ifndef GAUSSLEG_H
-//#define GAUSSLEG_H
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <lapacke.h>
-#include <cblas.h>
-#include <math.h>
+#include "gauleg.h"
 
 // function implementation
 void gauleg(int n, double* X, double* W){
@@ -38,38 +30,8 @@ void gauleg(int n, double* X, double* W){
 		E[i] = 0.5/sqrt(1.0 - pow(2.0*(i + 1.0), -2.0));
 	}
 		
-//	// print D and E
-//	printf("\nD = ");
-//	for (i = 0; i < n; i++)
-//		printf("%.6f ",D[i]);
-//	printf("\n");
-//
-//	printf("\nE = ");
-//	for (i = 0; i < n-1; i++)
-//		printf("%.6f ",E[i]);
-//	printf("\n");
-	
 	// compute eigenvalues and eigenvectors
 	info = LAPACKE_dstedc(LAPACK_ROW_MAJOR, 'I', n, D, E, Z, ldz);
-
-//	// print D, E, and Z
-//	printf("\nD = ");
-//	for (i = 0; i < n; i++)
-//		printf("%.6f ",D[i]);
-//	printf("\n");
-//
-//	printf("\nE = ");
-//	for (i = 0; i < n-1; i++)
-//		printf("%.6f ",E[i]);
-//	printf("\n");
-//	
-//	printf("\nZ = \n");
-//	for (j = 0; j < n; j++){
-//		for (i = 0; i < ldz; i++){
-//			printf("%.6f ", Z[i + j*n]);
-//		}
-//		printf("\n");
-//	}
 
 	// determine abscissas and associated weights on the interval [-1, 1]
 	for (i = 0; i < n; i++){
@@ -77,46 +39,9 @@ void gauleg(int n, double* X, double* W){
 		W[i] = 2*pow(Z[i], 2);
 	}
 
-//	// print X and W
-//	printf("\nX = ");
-//	for (i = 0; i < n; i++)
-//		printf("%.6f ",X[i]);
-//	printf("\n");
-//
-//	printf("\nW = ");
-//	for (i = 0; i < n; i++)
-//		printf("%.6f ",W[i]);
-//	printf("\n");
-
 	// release memory
 	free(D);
 	free(E);
 	free(Z);
 }
 
-int main(){
-	int n;
-	double *X, *W;
-	n = 5;
-
-	// note: have to make sure the range of integration is from -1 to 1, see Numerical Recipes p. 207 (p. 184 on pdf)
-
-	X = (double*) calloc(n,sizeof(double));
-	W = (double*) calloc(n,sizeof(double));
-	
-	gauleg(n, X, W);
-
-	// print abscissas and weights
-	int i;
-	printf("\nX = ");
-	for (i = 0; i < n; i++)
-		printf("%.6f ",X[i]);
-	printf("\n");
-
-	printf("\nW = ");
-	for (i = 0; i < n; i++)
-		printf("%.6f ",W[i]);
-	printf("\n");
-
-	return(0);
-}

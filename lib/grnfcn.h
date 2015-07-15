4c9917a44f41f76bd0a674d008596af2640459a6
jbarakat <jbarakat@stanford.edu>
2015-07-14 18:02:30 -0700

    worked on grnfcn for C

--- lib/grnfcn.h
+++ lib/grnfcn.h
@@ -92,13 +92,82 @@ void gf_axC(double x, double r, double x0, double r0, double rc,
 	// declare variables
 	int it, Nt;
 	double t, dt;
-	double omeg, omeg0, omegc;
+	double X = x - x0; 
+	double omeg, omeg0, omegc, omegn;
+	double I0, I1, I00, I10, I0c, I1c, I0n, I1n;
+	double K0, K1, K00, K10, K0c, K1c, K0n, K1n;
 	double Axx, Axr, Arx, Arr;
 	double Bxx, Bxr, Brx, Brr;
+	double Lxx, Lxr, Lrx, Lrr;
+	double Fxx, Fxr, Frx, Frr;
+	double detL, fc;
+	double cosXt, sinXt;
 	
+	// integration parameters (step size, upper bound)
 	dt = 0.01;
 	Nt = 1000;
+	
 	for (it = 0; it < Nt; it++){
+		t = it*dt;
+		if (t == 0)
+			t = 0.00001;
+		
+		// scaled radius
+		omeg = t*r;
+		omeg0 = t*r0;
+		omegc = t*rc;
+		omegn = 2*omegc - omeg - omeg0;
+		
+		// Bessel functions
+		I0 = besselI(0, omeg);
+		I1 = besselI(1, omeg);
+		I00 = besselI(0, omeg0);
+		I10 = besselI(1, omeg0);
+		I0c = besselI(0, omegc);
+		I1c = besselI(1, omegc);
+		I0n = besselI(0, omegn);
+		I1n = besselI(1, omegn);
+		
+		K0 = besselK(0, omeg);
+		K1 = besselK(1, omeg);
+		K00 = besselK(0, omeg0);
+		K10 = besselK(1, omeg0);
+		K0c = besselK(0, omegc);
+		K1c = besselK(1, omegc);
+		K0n = besselK(0, omegn);
+		K1n = besselK(1, omegn);
+
+		// components of L matrix in La = 4b
+		Lxx = t*I0c;
+		Lxr = omegc*I1c + 2*I0c;
+		Lrx = t*I1c;
+		Lrr = omegc*I0c;
+		detL = Lxx*Lrr - Lxr*Lrx;
+		if (detL == 0)
+			printf("Error: matrix L is singular.\n");
+		fc = 4/detL; 
+
+		// components of B matrix
+		Bxx = (-2*K0c + omegc*K1c)*I00 - K0c*omeg0*I10;
+		Bxr =               -omegc*K0c + K1c*omeg0*I10;
+		Brx =           -omegc*K1c*I10 + K0c*omeg0*I00;
+		Brr = ( 2*K1c + omegc*K0c)*I10 - K1c*omeg0*I00;
+
+		// components of A matrix from inverting La = 4b
+		Axx = fc*( Lrr*Bxx - Lxr*Bxr);
+		Axr = fc*(-Lrx*Bxx + Lxx*Bxr);
+		Arx = fc*( Lrr*Brx - Lxr*Brr);
+		Arr = fc*(-Lrx*Brx + Lxx*Brr);
+
+		// components of F matrix
+		Fxx = t*I0*Axx + (omeg*I1 + 2*I0)*Axr;
+		Fxr = t*I0*Arx + (omeg*I1 + 2*I0)*Arr;
+		Frx = t*I1*Axr +  omeg*I0        *Axr;
+		Frr = t*I1*Arx +  omeg*I0        *Arr;
+
+		// regularize the xx integrand
+		Fxx = Fxx + 8*K0n;
+
 
 	}
 
